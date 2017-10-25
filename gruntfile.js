a02dd95bbef8681761a30641d6518c6c6ade03ef
daria marczak <marczak.ria@gmail.com>
2017-10-25 16:35:48 +0200

    Modified gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -5,8 +5,8 @@ module.exports = function(grunt) {
         }
     });
 
-grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint']);
 
 };
