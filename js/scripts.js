5eb0a97ef2f9466f9a16ba470d7d4ce61915c225
daria marczak <marczak.ria@gmail.com>
2017-10-25 21:41:08 +0200

    Added some comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-// scripts.js
+
+//alert('Hello world!');//
+
+/*
+This is
+a block comment
+*/
 
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+
+//This is a comment that
+//doesn't work the same way as the block one
